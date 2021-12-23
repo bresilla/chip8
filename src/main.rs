d2baf03098f75af3a74e5080aa7f267783b1f242
Trim Bresilla <trim.bresilla@gmail.com>
2021-12-23 23:03:53 +0100

    ADD: Basic read of game file to Vec<u8>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("other/games/INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+    println!("{:?}", data);
 }

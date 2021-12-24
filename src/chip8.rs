fe6ae98803d6f97f97d8433934c9065d60e76553
Trim Bresilla <trim.bresilla@gmail.com>
2021-12-24 09:39:03 +0100

    ADD: ship load rom working

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,13 +1,17 @@
 use crate::ram::Ram;
 
-struct Chip8 {
+pub struct Chip8 {
     ram: Ram
 }
 
 impl Chip8 {
-    fn new() -> Chip8 {
-        Chip8 {
-            ram: Ram::new()
+    pub fn new() -> Chip8 {
+        Chip8 { ram: Ram::new() }
+    }
+    pub fn load_rom(&mut self, data: &Vec<u8>){
+        let offset = 0x260;
+        for i in 0..data.len() {
+            self.ram.write_byte((offset + i) as u16, data[i]);
         }
-    } 
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use std::io::Read;
 mod ram;
 mod chip8;
 
+use chip8::Chip8;
+
 fn main() {
     let mut file = File::open("other/games/INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data);
     println!("{:?}", data);
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data)
 }

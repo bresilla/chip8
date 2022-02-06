d10f7b523a6c67a68e250593d989bde8bdefa24e
Trim Bresilla <trim.bresilla@gmail.com>
2022-02-06 13:52:01 +0100

    ADD: 0x6 match

--- src/cpu.rs
+++ src/cpu.rs
@@ -23,8 +23,7 @@ impl Cpu {
     }
 
     pub fn execute(&mut self, ram: &mut Ram) {
-        let hi =
-            ram.read_byte(self.pc) as u16;
+        let hi = ram.read_byte(self.pc) as u16;
         let lo = ram.read_byte(self.pc + 1) as u16;
         let instruction: u16 = (hi << 8) | lo;
         println!("--> Instruction: {:#X} --> hi:{:#X} lo:{:#X}", instruction, hi, lo);
@@ -35,24 +34,37 @@ impl Cpu {
         let x = (instruction & 0x0F00) >> 8;
         let y = (instruction & 0x00F0) >> 4;
 
-        println!("--> Values: nnn={:?}, nn={:?}, n={:?}, x={:?}, y={:?}", nnn, nn, n, x, y);
+        println!("--> Values: nnn={:#X}, nn={:#X}, n={:?}, x={:#X}, y={:#X}", nnn, nn, n, x, y);
         if self.pc == self.prev { panic!("{}", format!("COUNTER NOT INCREMENTED").black().on_truecolor(0, 255, 136)) }
         self.prev = self.pc;
 
         match (instruction & 0xF000) >> 12 {
             0x1 => {
                 self.pc = nnn;
-                println!("{} --> jump to: {:#X}", " FLOW ".black().on_truecolor(0, 255, 136), nnn)
+                println!("{} --> jump to NNN which is: {:#X}", " FLOW ".black().on_truecolor(0, 255, 136), nnn)
             }
             0x6 => {
+                //Vx = N
                 self.write_reg(x, nn);
                 self.increment_pc();
                 println!("{} --> set VX: {} to NN: {:#X}", " CONST ".black().on_truecolor(0, 136, 255), x, nn)
+            }
+            0x7 => {
+                //Vx += N
+                let r = self.vx[x as usize] + n as u8;
+                self.write_reg(x, r);
+                self.increment_pc();
+                println!("{} --> set VX: {} to VX+N: {:#X}", " CONST ".black().on_truecolor(0, 136, 255), x, r)
             }
             0xA => {
                 self.i = nnn;
                 self.increment_pc();
                 println!("{} --> set i: {} to NNN: {:#X}", " MEM ".black().on_truecolor(169, 105, 231), x, nn)
+            }
+            0xB => {
+                //PC = V0 + NNN 
+                self.pc = self.vx[0] as u16 + nnn;
+                println!("{} --> jump to V0 + NNN which is: {:#X}", " FLOW ".black().on_truecolor(0, 255, 136), self.pc)
             }
             0xC => {
                 let mut rng = thread_rng();
@@ -62,7 +74,7 @@ impl Cpu {
                 println!("{} --> set VX: {} to (RANDOM & NN): {:#X}", " RAND ".black().on_truecolor(198, 150, 69), x, (r&nn))
             }
             _ => {
-                println!("{} --> unrecognized instruction: {:#X}", " ERROR ".black().on_truecolor(212, 60, 58), instruction);
+                println!("{} --> unrecognized instruction: {:#X}\n", " ERROR ".black().on_truecolor(212, 60, 58), instruction);
                 unreachable!()
             }
 
